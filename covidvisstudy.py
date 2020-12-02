3e7aaeec7b101a3abc6ff014fb964ac6dfca51a2
Priyam Mohanty <priyam.mohanty@berkeley.edu>
2020-12-02 11:42:34 -0800

    Update covidvisstudy.py

--- covidvisstudy.py
+++ covidvisstudy.py
@@ -14,7 +14,7 @@ from functools import reduce
 from scripts.generate_trendlines import *
 
 
-testing = False
+testing = True
 
 # Hides first radio obutton option, which we set to "-"
 # Allows us to avoid a pre-selected value
